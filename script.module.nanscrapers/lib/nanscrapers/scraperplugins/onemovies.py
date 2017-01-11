da89554d046389fd4b6502573e61bacd11f26ea1
Midraal <black.death.slayer@gmail.com>
2017-01-11 17:15:01 +0100

    update nanscrapers to v0.5.10

--- script.module.nanscrapers/lib/nanscrapers/scraperplugins/onemovies.py
+++ script.module.nanscrapers/lib/nanscrapers/scraperplugins/onemovies.py
@@ -25,6 +25,7 @@ class Onemovies(Scraper):
         self.server_link = '/ajax/get_episodes/%s'
         self.direct_link = '/ajax/v2_load_episode/'
         self.embed_link = '/ajax/load_embed/'
+        self.session = requests.Session()
 
     def scrape_movie(self, title, year, imdb):
         try:
@@ -32,14 +33,15 @@ class Onemovies(Scraper):
             headers = {'User-Agent': random_agent()}
             # print("ONEMOVIES", headers)
             query = self.search_link % (urllib.quote_plus(
-                " ".join(title.translate(None, '"?:!@#$&-').replace("'", " ").split())))  # clean up string and remove double spaces
+                    " ".join(title.translate(None, '"?:!@#$&-').replace("'",
+                                                                        " ").split())))  # clean up string and remove double spaces
             query = urlparse.urljoin(self.base_link, query)
             cleaned_title = clean_title(title)
-            cleaned_title =  " ".join(cleaned_title.translate(None, '\'"?:!@#$&-').split())
+            cleaned_title = " ".join(cleaned_title.translate(None, '\'"?:!@#$&-').split())
             # print("ONEMOVIES", query)
             page = 1
             while True:
-                html = requests.get(query, headers=headers, timeout=30).content
+                html = self.session.get(query, headers=headers, timeout=30).content
                 if "no result found" in html.lower():
                     break
                 html = BeautifulSoup(html)
@@ -52,10 +54,9 @@ class Onemovies(Scraper):
                         href = str(link['href'])
                         info = str(link['data-url'])
                         # print("ONEMOVIES", link_title, href, info)
-                        cleaned_link_title =  " ".join(clean_title(link_title).translate(None, '\'"?:!@#$&-').split())
-                        xbmc.log(cleaned_link_title + " vs " + cleaned_title)
+                        cleaned_link_title = " ".join(clean_title(link_title).translate(None, '\'"?:!@#$&-').split())
                         if cleaned_link_title == cleaned_title:
-                            html = requests.get(info, headers=headers).content
+                            html = self.session.get(info, headers=headers).content
                             pattern = '<div class="jt-info">%s</div>' % year
                             match = re.findall(pattern, html)
                             if match:
@@ -79,7 +80,7 @@ class Onemovies(Scraper):
             cleaned_title = clean_title(title)
             checkseason = cleaned_title + "season" + season
             # print("ONEMOVIES", query,checkseason)
-            html = BeautifulSoup(requests.get(query, headers=headers, timeout=30).content)
+            html = BeautifulSoup(self.session.get(query, headers=headers, timeout=30).content)
             containers = html.findAll('div', attrs={'class': 'ml-item'})
             for result in containers:
                 links = result.findAll('a')
@@ -100,6 +101,7 @@ class Onemovies(Scraper):
         return []
 
     def sources(self, url):
+        original_url = url
         sources = []
         try:
             # print("ONEMOVIES SOURCES", url)
@@ -108,13 +110,16 @@ class Onemovies(Scraper):
             referer = url
             headers = {'User-Agent': random_agent()}
             url = url.replace('/watching.html', '')
-            html = requests.get(url, headers=headers).content
+            request = self.session.get(url, headers=headers)
+            html = request.content
             # print ("ONEMOVIES Source", html)
             try:
                 url, episode = re.findall('(.+?)\?episode=(\d*)$', url)[0]
             except:
                 episode = None
             vid_id = re.findall('-(\d+)', url)[-1]
+            getc = re.findall(r'<img title=.*?src="(.*?)"', html, re.I | re.DOTALL)[0]
+            cookie = get_cookie(getc, original_url, self.session)
             # print ("ONEMOVIES", vid_id)
             quality = re.findall('<span class="quality">(.*?)</span>', html)
             quality = str(quality)
@@ -127,79 +132,91 @@ class Onemovies(Scraper):
             try:
                 headers = {'X-Requested-With': 'XMLHttpRequest'}
                 headers['Referer'] = referer
-                headers['User-Agent'] = random_agent()
+                headers[
+                    'User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36'
                 u = urlparse.urljoin(self.base_link, self.server_link % vid_id)
                 # print("SERVERS", u)
-                r = BeautifulSoup(requests.get(u, headers=headers).content)
+                request = self.session.get(u, headers=headers)
+                r = BeautifulSoup(request.content)
                 # print("SERVERS", r)
                 containers = r.findAll('div', attrs={'class': 'les-content'})
                 for result in containers:
-                    links = result.findAll('a')
-                    # print("ONEMOVIES", links)
-                    for link in links:
-                        title = str(link['title'])
-                        # print("ONEMOVIES TITLE", title)
-                        if not episode == None:
-                            title = re.findall('Episode\s+(\d+):', title)[0]
-                            title = '%01d' % int(title)
-                            if title == episode:
-                                episode_id = str(link['episode-id'])
-                            # print("ONEMOVIES EPISODE", episode_id)
+                    try:
+                        links = result.findAll('a')
+                        # print("ONEMOVIES", links)
+                        for link in links:
+                            title = str(link['title'])
+                            # print("ONEMOVIES TITLE", title)
+                            if not episode == None:
+                                title = re.findall('Episode\s+(\d+):', title)[0]
+                                title = '%01d' % int(title)
+                                if title == episode:
+                                    episode_id = str(link['episode-id'])
+                                # print("ONEMOVIES EPISODE", episode_id)
+                                else:
+                                    continue
+
                             else:
-                                continue
+                                episode_id = str(link['episode-id'])
+                            onclick = str(link['onclick'])
 
-                        else:
-                            episode_id = str(link['episode-id'])
-                        onclick = str(link['onclick'])
+                            key_gen = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(16))
+                            ################# FIX FROM MUCKY DUCK & XUNITY TALK ################
+                            key = '87wwxtp3dqii'
+                            key2 = '7bcq9826avrbi6m49vd7shxkn985mhod'
+                            coookie = hashlib.md5(episode_id + key).hexdigest() + '=%s' % key_gen
+                            cookie = '%s; %s' % (cookie, coookie)
+                            a = episode_id + key2
+                            b = key_gen
+                            # i = b[-1]
+                            # h = b[:-1]
+                            # b = i + h + i + h + i + h
+                            hash_id = uncensored(a, b)
+                            ################# FIX FROM MUCKY DUCK & XUNITY TALK ################
 
-                        key_gen = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(16))
-                        ################# FIX FROM MUCKY DUCK & XUNITY TALK ################
-                        key = '87wwxtp3dqii'
-                        key2 = '7bcq9826avrbi6m49vd7shxkn985mhod'
-                        cookie = hashlib.md5(episode_id + key).hexdigest() + '=%s' % key_gen
-                        a = episode_id + key2
-                        b = key_gen
-                        i = b[-1]
-                        h = b[:-1]
-                        b = i + h + i + h + i + h
-                        hash_id = uncensored(a, b)
-                        ################# FIX FROM MUCKY DUCK & XUNITY TALK ################
+                            serverurl = self.base_link + '/ajax/v2_get_sources/' + episode_id + '?hash=' + urllib.quote(
+                                    hash_id)
+                            # print ("playurl ONEMOVIES", serverurl)
 
-                        serverurl = self.base_link + '/ajax/v2_get_sources/' + episode_id + '?hash=' + urllib.quote(
-                                hash_id)
-                        # print ("playurl ONEMOVIES", serverurl)
+                            headers = {'Accept-Language' : 'en-US',
+                                       'Accept-Encoding' : 'gzip, deflate, sdch',
+                                       'Cookie'          : cookie,
+                                       'Referer'         : referer,
+                                       'x-requested-with': 'XMLHttpRequest',
+                                       'Accept'          : 'application/json, text/javascript, */*; q=0.01',
+                                       'User-Agent'      : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36',
+                                       }
+                            # print ("playurl ONEMOVIES", headers)
+                            result = self.session.get(serverurl, headers=headers).content
+                            # print ("RESULT ONEMOVIES", result)
+                            result = result.replace('\\', '')
+                            # print ("ONEMOVIES Result", result)
+                            url = re.findall('"?file"?\s*:\s*"(.+?)"', result)
+                            url = [googletag(i) for i in url]
+                            url = [i[0] for i in url if len(i) > 0]
+                            u = []
+                            try:
+                                u += [[i for i in url if i['quality'] == '1080p'][0]]
+                            except:
+                                pass
+                            try:
+                                u += [[i for i in url if i['quality'] == '720'][0]]
+                            except:
+                                pass
+                            try:
+                                u += [[i for i in url if i['quality'] == '480'][0]]
+                            except:
+                                pass
+                            url = replaceHTMLCodes(u[0]['url'])
+                            quality = googletag(url)[0]['quality']
 
-                        headers = {'Accept-Language' : 'en-US', 'Cookie': cookie, 'Referer': referer,
-                                   'User-Agent'      : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.89 Safari/537.36',
-                                   'X-Requested-With': 'XMLHttpRequest'}
-                        # print ("playurl ONEMOVIES", headers)
-                        result = requests.get(serverurl, headers=headers).content
-                        # print ("RESULT ONEMOVIES", result)
-                        result = result.replace('\\', '')
-                        # print ("ONEMOVIES Result", result)
-                        url = re.findall('"?file"?\s*:\s*"(.+?)"', result)
-                        url = [googletag(i) for i in url]
-                        url = [i[0] for i in url if len(i) > 0]
-                        u = []
-                        try:
-                            u += [[i for i in url if i['quality'] == '1080p'][0]]
-                        except:
-                            pass
-                        try:
-                            u += [[i for i in url if i['quality'] == '720'][0]]
-                        except:
-                            pass
-                        try:
-                            u += [[i for i in url if i['quality'] == '480'][0]]
-                        except:
-                            pass
-                        url = replaceHTMLCodes(u[0]['url'])
-                        quality = googletag(url)[0]['quality']
+                            # print ("ONEMOVIES PLAY URL", quality, url)
 
-                        # print ("ONEMOVIES PLAY URL", quality, url)
-
-                        sources.append({'source': 'google video', 'quality': quality, 'scraper': self.name, 'url': url,
-                                        'direct': True})
+                            sources.append(
+                                    {'source': 'google video', 'quality': quality, 'scraper': self.name, 'url': url,
+                                     'direct': True})
+                    except:
+                        pass
             except:
                 pass
 
@@ -216,23 +233,30 @@ class Onemovies(Scraper):
         return xml
 
 
+def __jav(a):
+    b = str(a)
+    code = ord(b[0])
+    if 0xD800 <= code and code <= 0xDBFF:
+        c = code
+        if len(b) == 1:
+            return code
+        d = ord(b[1])
+        return ((c - 0xD800) * 0x400) + (d - 0xDC00) + 0x10000
+
+    if 0xDC00 <= code and code <= 0xDFFF:
+        return code
+    return code
+
+
 def uncensored(a, b):
-    n = -1
-    fuckme = []
-    justshow = []
-    while True:
+    c = ''
+    i = 0
+    for i, d in enumerate(a):
+        e = b[i % len(b) - 1]
+        d = int(__jav(d) + __jav(e))
+        c += chr(d)
 
-        if n == len(a) - 1:
-            break
-        n += 1
-
-        d = int(''.join(str(ord(c)) for c in a[n]))
-
-        e = int(''.join(str(ord(c)) for c in b[n]))
-        justshow.append(d + e)
-        fuckme.append(chr(d + e))
-
-    return base64.b64encode(''.join(fuckme))
+    return base64.b64encode(c)
 
 
 def googletag(url):
@@ -255,3 +279,18 @@ def googletag(url):
         return [{'quality': '480', 'url': url}]
     else:
         return []
+
+
+def get_cookie(url, referer, session):
+    headers = {'Accept'         : 'image/webp,image/*,*/*;q=0.8',
+               'Accept-Encoding': 'gzip, deflate, sdch, br',
+               'Accept-Language': 'en-US,en;q=0.8',
+               'Referer'        : referer,
+               'User-Agent'     : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+               }
+    request = session.get(url, headers=headers)
+    cookie = request.cookies.get_dict()
+    newcookie = ""
+    for i in cookie:
+        newcookie = i + '=' + cookie[i]
+    return newcookie
